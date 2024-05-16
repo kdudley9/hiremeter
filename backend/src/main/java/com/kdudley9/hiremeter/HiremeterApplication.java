db06f86e0e26cc07cda14e5d9b8bd9491fba3e46
kdudley9 <98002234+kdudley9@users.noreply.github.com>
2024-05-15 23:27:20 -0400

    initial commit

---
+++ backend/src/main/java/com/kdudley9/hiremeter/HiremeterApplication.java
@@ -0,0 +1,13 @@
+package com.kdudley9.hiremeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiremeterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiremeterApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/kdudley9/hiremeter/HiremeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdudley9.hiremeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiremeterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
