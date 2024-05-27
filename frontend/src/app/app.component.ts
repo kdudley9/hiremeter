cbbc3584fd129cfd1fda9127dac7c49fab814610
kdudley9 <98002234+kdudley9@users.noreply.github.com>
2024-05-27 19:55:33 -0400

    Add styling to landing page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SiteHeaderComponent } from "./shared_components/site-header/site-header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, SiteHeaderComponent]
 })
 export class AppComponent {
   title = 'frontend';
